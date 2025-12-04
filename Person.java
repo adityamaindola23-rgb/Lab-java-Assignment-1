52b8b7751d351d65f46a39658d82ef67857fefcd
adityamaindola23-rgb <adityamaindola23@gmail.com>
2025-12-04 10:52:32 +0530

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,14 @@
+public class Person {
+
+    protected String name;
+
+    // Default Constructor
+    public Person() {
+        this.name = "Unknown";
+    }
+
+    // Parameterized Constructor
+    public Person(String name) {
+        this.name = name;
+    }
+}
